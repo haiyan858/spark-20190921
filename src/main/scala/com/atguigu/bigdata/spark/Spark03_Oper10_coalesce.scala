9a0dcade05eddee0a2a13dfb68b1b2ed92d3fd4c
cuihaiyan <haiyan.cui@zebra-c.com>
2019-09-27 14:45:01 +0800

    coalesce 缩减分区的数量，可简单理解为合并分区

---
+++ src/main/scala/com/atguigu/bigdata/spark/Spark03_Oper10_coalesce.scala
@@ -0,0 +1,26 @@
+package com.atguigu.bigdata.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * coalesce 缩减分区的数量，可简单理解为合并分区
+  *
+  * @Author cuihaiyan
+  * @Create_Time 2019-09-27 14:19
+  */
+object Spark03_Oper10_coalesce {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sparkContext = new SparkContext(conf)
+
+    val listRDD: RDD[Int] = sparkContext.makeRDD(1 to 16, 4)
+    println("缩减分区前：" + listRDD.partitions.size)
+
+    val coalesceRDD: RDD[Int] = listRDD.coalesce(3)
+    println("缩减分区后：" + coalesceRDD.partitions.size)
+
+
+  }
+}
