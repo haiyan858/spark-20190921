e6b13fea451869a1316608584dbbb683b6a864a6
cuihaiyan <haiyan.cui@zebra-c.com>
2019-09-27 14:16:33 +0800

    glom() 将一个分区的数据放到一个数组

--- src/main/scala/com/atguigu/bigdata/spark/Spark02_Oper5_glom.scala
+++ src/main/scala/com/atguigu/bigdata/spark/Spark02_Oper5_glom.scala
@@ -15,9 +15,10 @@ object Spark02_Oper5_glom {
     val config: SparkConf = new SparkConf().setMaster("local[*]").setAppName("wordCount")
     val sc = new SparkContext(config)
 
-    //创建一个4个分区的RDD，并将每个分区的数据放到一个数组
+    //创建一个4个分区的RDD，并将一个分区的数据放到一个数组
     //each partition into an array
-    val listRDD: RDD[Int] = sc.makeRDD(1 to 17, 4)
+    //val listRDD: RDD[Int] = sc.makeRDD(1 to 17, 4)
+    val listRDD: RDD[Int] = sc.makeRDD(List(1,2,3,4,4,5,6,7,7,8), 4)
 
     /*val indexRDD: RDD[(Int, String)] = listRDD.mapPartitionsWithIndex {
       case (index, datas) => {
@@ -29,9 +30,12 @@ object Spark02_Oper5_glom {
     val glomRDD: RDD[Array[Int]] = listRDD.glom()
 
     glomRDD.collect().foreach(array => {
-      for (elem <- array) {
+      /*for (elem <- array) {
         println(elem)
-      }
+      }*/
+      //println(array.mkString(","))
+      //max值
+      println(array.max)
     })
 
   }
