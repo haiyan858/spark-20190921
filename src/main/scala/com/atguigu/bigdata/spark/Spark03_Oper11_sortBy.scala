d49f65b1a6c145d5e7300c9b02a8b09f113b88b5
cuihaiyan <haiyan.cui@zebra-c.com>
2019-09-27 20:19:22 +0800

    sortBy 排序

---
+++ src/main/scala/com/atguigu/bigdata/spark/Spark03_Oper11_sortBy.scala
@@ -0,0 +1,27 @@
+package com.atguigu.bigdata.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * sortBy 按照给定的规则排序， 默认正序
+  *
+  * @Author cuihaiyan
+  * @Create_Time 2019-09-27 20:19
+  */
+object Spark03_Oper11_sortBy {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sparkContext = new SparkContext(conf)
+
+    val listRDD: RDD[Int] = sparkContext.makeRDD(1 to 16, 4)
+    //listRDD.sortBy(x=>x).collect.foreach(println)
+    //对3取余数的大小排序
+    listRDD.sortBy(x => x % 3).collect.foreach(println)
+
+    //停止：关闭资源
+    sparkContext.stop()
+
+  }
+}
