7e33ceaf8c695ec8f32205e33fbaa69cf80d4a3f
cuihaiyan <haiyan.cui@zebra-c.com>
2019-10-09 11:31:27 +0800

    Action算子

--- src/main/scala/com/atguigu/bigdata/spark/rdd/actions/Spark06_Oper4_take.scala
+++ src/main/scala/com/atguigu/bigdata/spark/rdd/actions/Spark06_Oper4_take.scala
@@ -1,21 +1,29 @@
 package com.atguigu.bigdata.spark.rdd.actions
 
-
 /**
   * @Author cuihaiyan
   * @Create_Time 2019-10-09 11:23
   */
-object Spark06_Oper2_count {
+object Spark06_Oper4_take {
 
-  //count()
-  //返回rdd中的元素个数
+  //take(n)
+  //返回rdd中前n个元素，数组的形式返回
 
   /**
     * scala> val rdd = sc.parallelize(1 to 10)
     * rdd: org.apache.spark.rdd.RDD[Int] = ParallelCollectionRDD[4] at parallelize at <console>:24
-    * *
+    *
     * scala> rdd.count()
     * res6: Long = 10
+    *
+    * scala> rdd.first()
+    * res8: Int = 1
+    *
+    * scala> rdd.take(3)
+    * res9: Array[Int] = Array(1, 2, 3)
+    *
+    * scala> rdd.take(5)
+    * res10: Array[Int] = Array(1, 2, 3, 4, 5)
     *
     */
 }
