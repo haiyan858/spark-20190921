b5f6fb362b55be42014e0fb0482142c504253554
cuihaiyan <haiyan.cui@zebra-c.com>
2019-10-16 21:18:14 +0800

    RDD checkpoint

--- src/main/scala/com/atguigu/bigdata/spark/rdd/Spark08_Oper1_Checkpoint.scala
+++ src/main/scala/com/atguigu/bigdata/spark/rdd/Spark08_Oper1_Checkpoint.scala
@@ -15,15 +15,32 @@ object Spark08_Oper1_Checkpoint {
     val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
     val sc = new SparkContext(conf)
 
+    //设置检查点的保存目录
+    //Checkpoint directory has not been set in the SparkContext
+    sc.setCheckpointDir("cp")
+
     val rdd: RDD[Int] = sc.makeRDD(List(1,2,3,4))
 
     val mapRDD: RDD[(Int, Int)] = rdd.map((_,1))
+
+    //checkpoint
     mapRDD.checkpoint()
 
     val reduceRDD: RDD[(Int, Int)] = mapRDD.reduceByKey(_+_)
 
+    reduceRDD.foreach(println)
     println(reduceRDD.toDebugString)
+    /*
+    (4) ShuffledRDD[2] at reduceByKey at Spark08_Oper1_Checkpoint.scala:22 []
+    +-(4) MapPartitionsRDD[1] at map at Spark08_Oper1_Checkpoint.scala:20 []
+    |  ParallelCollectionRDD[0] at makeRDD at Spark08_Oper1_Checkpoint.scala:18 []
+    */
 
+    /*
+    (4) ShuffledRDD[2] at reduceByKey at Spark08_Oper1_Checkpoint.scala:29 []
+    +-(4) MapPartitionsRDD[1] at map at Spark08_Oper1_Checkpoint.scala:24 []
+    |  ReliableCheckpointRDD[3] at foreach at Spark08_Oper1_Checkpoint.scala:31 []
+    */
     //释放资源
     sc.stop()
   }
