9efec10803aef01edff7713e98b6d0942beba1c4
cuihaiyan <haiyan.cui@zebra-c.com>
2019-11-07 23:21:21 +0800

    弱类型用户自定义聚合函数

--- src/main/scala/com/atguigu/bigdata/spark/sql/MyAverage.scala
+++ src/main/scala/com/atguigu/bigdata/spark/sql/MyAverage.scala
@@ -2,7 +2,7 @@ package com.atguigu.bigdata.spark.sql
 
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-import org.apache.spark.sql.types.{DataType, StructType}
+import org.apache.spark.sql.types.{DataType, DoubleType, LongType, StructField, StructType}
 
 /**
   * 求平均工资的自定义聚合函数
@@ -11,19 +11,44 @@ import org.apache.spark.sql.types.{DataType, StructType}
   * @Create_Time 2019-11-07 22:58
   */
 object MyAverage extends UserDefinedAggregateFunction{
-  override def inputSchema: StructType = ???
 
-  override def bufferSchema: StructType = ???
+  // 聚合函数输入参数的数据类型
+  override def inputSchema: StructType = StructType(StructField("inputColumn", LongType) :: Nil)
 
-  override def dataType: DataType = ???
+  // 聚合缓冲区中值得数据类型
+  override def bufferSchema: StructType = {
+    StructType(StructField("sum", LongType) :: StructField("count", LongType) :: Nil)
+  }
 
-  override def deterministic: Boolean = ???
+  // 返回值的数据类型
+  override def dataType: DataType = DoubleType
 
-  override def initialize(buffer: MutableAggregationBuffer): Unit = ???
+  // 对于相同的输入是否一直返回相同的输出。
+  override def deterministic: Boolean = true
 
-  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = ???
+  // 初始化
+  override def initialize(buffer: MutableAggregationBuffer): Unit = {
+    // 存工资的总额
+    buffer(0) = 0L
+    // 存工资的个数
+    buffer(1) = 0L
+  }
 
-  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = ???
+  // 相同Execute间的数据合并。
+  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+    if (!input.isNullAt(0)) {
+      buffer(0) = buffer.getLong(0) + input.getLong(0)
+      buffer(1) = buffer.getLong(1) + 1
+    }
+  }
 
-  override def evaluate(buffer: Row): Any = ???
+  // 不同Execute间的数据合并
+  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+    buffer1(0) = buffer1.getLong(0) + buffer2.getLong(0)
+    buffer1(1) = buffer1.getLong(1) + buffer2.getLong(1)
+  }
+
+  // 计算最终结果
+  override def evaluate(buffer: Row): Double = buffer.getLong(0).toDouble / buffer.getLong(1)
 }
+
--- src/main/scala/com/atguigu/bigdata/spark/sql/SparkSQL05_UDF.scala
+++ src/main/scala/com/atguigu/bigdata/spark/sql/SparkSQL05_UDF.scala
@@ -1,7 +1,11 @@
 package com.atguigu.bigdata.spark.sql
 
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
 /**
-  *
+  * 弱类型用户自定义聚合函数：
+  * 通过继承UserDefinedAggregateFunction来实现用户自定义聚合函数
   *
   * @Author cuihaiyan
   * @Create_Time 2019-11-07 22:53
@@ -25,6 +29,32 @@ object SparkSQL05_UDF {
 //        |       NameJustin|  19|
 //        +-----------------+----+
 
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkSQL05_UDF")
+
+    val spark: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+
+    // 注册函数
+    spark.udf.register("myAverage", MyAverage)
+
+    val df = spark.read.json("data/employees.json")
+    df.createOrReplaceTempView("employees")
+    df.show()
+    //+-------+------+
+    //|   name|salary|
+    //+-------+------+
+    //|Michael|  3000|
+    //|   Andy|  4500|
+    //| Justin|  3500|
+    //|  Berta|  4000|
+    //+-------+------+
+
+    val result = spark.sql("SELECT myAverage(salary) as average_salary FROM employees")
+    result.show()
+    //+--------------+
+    //|average_salary|
+    //+--------------+
+    //|        3750.0|
+    //+--------------+
 
   }
 
